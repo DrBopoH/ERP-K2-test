cc756366949617f7999a93b8509eb53fa708104d
DrBopoH <lecrowz666@gmail.com>
2026-06-02 17:10:15 +0300

    Frontend TS typing

---
+++ source/frontend/src/types/index.ts
@@ -0,0 +1,28 @@
+export interface Client {
+	id: number
+	name: string
+}
+
+export interface Product {
+	id: number
+	name: string
+	price: number
+}
+
+export interface OrderItem {
+	product_id: number
+	quantity: number
+}
+
+export interface Order {
+	id: number
+	client_id: number
+	total_amount: number
+	created_at: string
+	items: {
+		product_id: number
+		product_name: string
+		quantity: number
+		price_at_moment: number
+	}[]
+}
